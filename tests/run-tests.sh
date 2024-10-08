11ec7a0d5c37d0b6e129e432c8230a1795c7f6c4
Cody Allen <cody.allen@unison.cloud>
2024-10-08 14:39:21 -0400

    Fix CI

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -6,7 +6,18 @@ TRANSCRIPT=pull-and-run-tests.md
 
 envsubst < pull-and-run-tests.tpl.md > $TRANSCRIPT
 
-curl -L "https://github.com/unisonweb/unison/releases/download/${UCM_RELEASE}/ucm-linux.tar.gz" | tar -xz
+release_file_name() {
+  case "$UCM_RELEASE" in
+    "release%2F0.5.23"  | "release%2F0.5.27")
+      echo "ucm-linux.tar.gz"
+      ;;
+    *)
+      echo "ucm-linux-x64.tar.gz"
+      ;;
+  esac
+}
+
+curl --fail-with-body -L "https://github.com/unisonweb/unison/releases/download/${UCM_RELEASE}/$(release_file_name)" | tar -xz
 
 exit_status=0
 
